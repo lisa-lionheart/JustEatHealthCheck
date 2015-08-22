778d0c04c9e957c36affed4f61831fbf4009cad2
Lisa Croxford <lisa-lionheart@users.noreply.github.com>
2015-08-22 18:26:42 +0100

    Update JEHealthCheck.user.js

--- JEHealthCheck.user.js
+++ JEHealthCheck.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  enter something useful
 // @author       Lisa Croxford
-// @match        http://www.just-eat.co.uk/*
+// @match        *//www.just-eat.co.uk/*
 // @grant        none
 // ==/UserScript==
 
